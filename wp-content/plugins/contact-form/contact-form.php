1137d323e3fae0c9ec3a879ee936d7f2d3265d12
Kamil Mendelski <kamil@frontlabs.pl>
2019-11-22 12:13:32 +0100

    Version bump to plugin

--- wp-content/plugins/contact-form/contact-form.php
+++ wp-content/plugins/contact-form/contact-form.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     contact-form
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * @package         Contact_Form
  */
